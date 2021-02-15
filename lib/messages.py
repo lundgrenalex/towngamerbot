e7333ebaddd6f89b1078d0d00fc603516cb74043
Alex <alex@endem.su>
2021-02-15 18:51:54 +0300

    fix answered message

--- lib/messages.py
+++ lib/messages.py
@@ -20,6 +20,44 @@ def process(message: dict):
         return
     process_message(message)
 
+def bot_answer(message: dict):
+
+    chat_id = message['message']['chat']['id']
+
+    bot_answer = game.get_new_answer(message)
+    if not bot_answer:
+
+        # Get score for username
+        score = game.get_score(chat_id)
+        username = telegram.get_chat(
+            chat_id,
+            bot_token=telegram_key)['result']['username']
+
+        telegram.send_message(
+            message['message']['chat']['id'],
+            message=helpers.render_template('winner', username, score),
+            bot_token=telegram_key)
+
+        # Cancel game
+        game.cancel(chat_id)
+
+        return False
+
+    result = telegram.send_message(
+        message['message']['chat']['id'],
+        message=bot_answer,
+        bot_token=telegram_key
+    )
+    logging.info(result)
+
+    # write bot answer
+    result = game.save_bot_answer(result)
+    logging.info(result)
+
+    return True
+
+    pass
+
 def process_message(message: dict):
 
     # BUGFIX: to lower text
@@ -55,12 +93,7 @@ def process_message(message: dict):
 
     # last_answer uid =! uid
     if last_answer['user_id'] == message['message']['from']['id']:
-        result = telegram.send_message(
-            chat_id,
-            message='Вы уже отвечали, ждите ответа бота!',
-            bot_token=telegram_key
-        )
-        return False
+        return bot_answer(message)
 
     # check last symbol from last answer
     if not helpers.is_word_in_chain(last_answer['message'], message['message']['text']):
@@ -105,34 +138,4 @@ def process_message(message: dict):
         return False
 
     # get bot answer
-    bot_answer = game.get_new_answer(message)
-    if not bot_answer:
-
-        # Get score for username
-        score = game.get_score(chat_id)
-        username = telegram.get_chat(
-            chat_id,
-            bot_token=telegram_key)['result']['username']
-
-        telegram.send_message(
-            message['message']['chat']['id'],
-            message=helpers.render_template('winner', username, score),
-            bot_token=telegram_key)
-
-        # Cancel game
-        game.cancel(chat_id)
-
-        return False
-
-    result = telegram.send_message(
-        message['message']['chat']['id'],
-        message=bot_answer,
-        bot_token=telegram_key
-    )
-    logging.info(result)
-
-    # write bot answer
-    result = game.save_bot_answer(result)
-    logging.info(result)
-
-    return True
+    return bot_answer(message)
