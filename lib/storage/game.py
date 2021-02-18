1b8110066b317be2de7f0245f878bd564eff705c
Alex <alex@endem.su>
2021-02-18 12:34:56 +0300

    Add sorting by population and ane bug fixed

--- lib/storage/game.py
+++ lib/storage/game.py
@@ -38,7 +38,10 @@ def is_answered_city(message: dict):
     db = mongo.connect()
     return db.bot.game.find_one({
         'chat_id': message['message']['chat']['id'],
-        'message': message['message']['text'].lower(),
+        'message': {
+            '$regex': f'^{ message['message']['text']}',
+            '$options' : 'i'
+        }
     })
 
 def get_last_answer(message: dict):
@@ -53,17 +56,14 @@ def get_last_answer(message: dict):
 
 def get_new_answer(message: dict):
     db = mongo.connect()
-    city_name = helpers.normalize_city_name(message['message']['text'])
     last_simbol = list(city_name)[-1:][0]
-    last_answers = db.bot.game.find({'chat_id': message['message']['chat']['id']})
-    last_answers = [a['message'] for a in last_answers]
-    cities = db.bot.cities.find({'$and': [
-        {'city': {'$regex': f'^{last_simbol}', '$options' : 'i'}},
-        {'city': {'$nin': last_answers}}
-    ]})
+    cities = db.bot.cities.find({
+        'city': {
+            '$regex': f'^{last_simbol}',
+            '$options' : 'i'}}).sort('population', pymongo.DESCENDING)]).limit(1)
     try:
-        cities = [c['city'] for c in cities]
-        return random.choice(cities)
+        cities = [c['city'] for c in cities][0]
+        return cities[0]
     except (TypeError, IndexError):
         return False
 
@@ -74,7 +74,8 @@ def get_score(chat_id: int) -> int:
 
     # get keys
     for a in answers:
-        score += (1 / db.bot.cities.count({'city': {'$regex': f'^{a[0]}', '$options' : 'i'}}))
+        score += (1 / db.bot.cities.count({
+            'city': {'$regex': f'^{a[0]}', '$options' : 'i'}}))
 
     # return real score
     return int(score * 1000)
