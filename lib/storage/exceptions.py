9a19074ab5abc02c7b9f3a60198680ccbaa7a517
Alex <alex@endem.su>
2021-02-25 20:07:30 +0300

    fix exception

--- lib/storage/exceptions.py
+++ lib/storage/exceptions.py
@@ -1,11 +1,5 @@
 class GameError(Exception):
 
-    def __init__(self, message=None):
-        self.message = message or self.message
-        assert self.message, 'Message not implemented!'
-        super(GameError, self).__init__(message)
-
-    def __str__(self):
-        if self.internal_message:
-            return f'{self.message}, {self.internal_message}'
-        return self.message
+    def __init__(self, message=""):
+        self.message = message
+        super().__init__(self.message)
---
+++ test_exception.py
@@ -0,0 +1,4 @@
+from lib.storage.exceptions import GameError
+
+if '__main__':
+    raise GameError(message="Game don't exists")
